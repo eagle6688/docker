77415ca5d09b6bd1f74cc9e8c2cbb4d6e638413f
Su Liang <fangyansu@163.com>
2018-01-14 11:51:16 +0800

    update

---
+++ etcd/CMD.sh
@@ -0,0 +1,14 @@
+docker run \
+  -it \
+  -p 2379:2379 \
+  -p 2380:2380 \
+  -v /var/data/etcd:/data \
+  --name etcd-master \
+  eagle6688/etcd \
+  --name etcd-master \
+  -–data-dir '/data' \
+  -–listen-client-urls http://0.0.0.0:2379
+  --initial-advertise-peer-urls http://10.100.97.46:2380 \
+  -–initial-cluster 'master=http://10.100.97.46:2380,G510=http://10.100.97.64:2380' \
+  -–initial-cluster-token eagle-cluster \
+  --advertise-client-urls http://10.100.97.46:2379
--- etcd/run.sh
+++ etcd/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # Check for $CLIENT_URLS
 if [ -z ${LISTEN_URLS+x} ]; then
         LISTEN_URLS="http://0.0.0.0:2379"
@@ -15,7 +16,7 @@ else
         echo "Detected new ADVERTISE_URLS value of $ADVERTISE_URLS"
 fi
 
-ETCD_CMD="/bin/etcd -data-dir=/data --advertise-client-urls=${ADVERTISE_URLS} -listen-client-urls=${LISTEN_URLS} $*"
+ETCD_CMD="/bin/etcd --data-dir='/data' --advertise-client-urls=${ADVERTISE_URLS} --listen-client-urls=${LISTEN_URLS} $*"
 echo -e "Running '$ETCD_CMD'\nBEGIN ETCD OUTPUT\n"
 
 exec $ETCD_CMD
