4dd51b6474e2988299435684ed836a6a1272454a
su liang <fangyansu@163.com>
2018-01-16 12:26:49 +0800

    update CMD.sh

--- etcd/CMD.sh
+++ etcd/CMD.sh
@@ -38,3 +38,20 @@ docker run \
   --initial-cluster-state existing \
   --initial-cluster-token eagle-cluster \
   --advertise-client-urls http://10.100.97.64:2379
+
+  docker run \
+  -it \
+  -p 2379:2379 \
+  -p 2380:2380 \
+  -v /var/data/etcd:/data \
+  --name etcd-XIAOXIN310 \
+  eagle6688/etcd \
+  --name etcd-XIAOXIN310 \
+  --data-dir /data \
+  --listen-peer-urls http://0.0.0.0:2380 \
+  --listen-client-urls http://0.0.0.0:2379 \
+  --initial-advertise-peer-urls http://10.100.97.202:2380 \
+  --initial-cluster etcd-master=http://10.100.97.46:2380,etcd-G510=http://10.100.97.64:2380,etcd-XIAOXIN310=http://10.100.97.202:2380 \
+  --initial-cluster-state existing \
+  --initial-cluster-token eagle-cluster \
+  --advertise-client-urls http://10.100.97.202:2379
